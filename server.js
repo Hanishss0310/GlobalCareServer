68437ec010f0dddf4ea41094a949ae0d90185437
Hanish SS <hanishgowda7795@gmail.com>
2025-07-23 14:46:17 +0530

    Scond

--- server.js
+++ server.js
@@ -34,13 +34,37 @@ if (!fs.existsSync(uploadDir)) {
 
 // Express setup
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  'https://globaljpkp20caresurgisals746jj.firebaseapp.com',
+  'https://globaljpkp20caresurgisals746jj.web.app',
+  'https://globalcaresurgical.in',
+  'https://secureapi-b8p3vzk19x.globalcaresurgical.in',
+  'https://global-care-surgicals-user-app.firebaseapp.com'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('❌ Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static(uploadDir));
 
 // Security middlewares
 app.use(helmet());
 // app.use(xss());
+
+
+app.get('/', (req, res) => {
+  res.send('✅ GlobalCare API is running');
+});
 
 // MongoDB connection
 mongoose.connect('mongodb://127.0.0.1:27017/GlobalCare')
