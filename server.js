227a0aea2a975a1dcbefb20dd4c3e48f5786c25a
Hanish SS <hanishgowda7795@gmail.com>
2025-07-24 13:13:51 +0530

    New submission

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
-const allowedOrigins = [
+cconst allowedOrigins = [
   'https://globaljpkp20caresurgisals746jj.firebaseapp.com',
   'https://globaljpkp20caresurgisals746jj.web.app',
   'https://globalcaresurgical.in',
@@ -48,21 +48,25 @@ const allowedOrigins = [
   'https://global-care-surgicals-user-app.firebaseapp.com'
 ];
 
-// Proper cors middleware configuration
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+// Custom CORS Middleware (✅ CORRECT)
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
 
-app.use(cors(corsOptions));
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 
 app.use(express.json());
